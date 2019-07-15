120742d43a772e36d62bf26628f66c2f075f89b7
AAYUSH MITTAL <aayushmittal1998@gmail.com>
2019-07-15 23:43:25 +0530

    main_updated

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"dashboard/controller"
-	"database/sql"
+	"dashboard/database"
+	"dashboard/handlers"
 	"flag"
 	"log"
 	"net/http"
@@ -11,28 +11,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var db *sql.DB
-var err error
-
 func main() {
 
 	r := mux.NewRouter()
 
-	db, err = sql.Open("mysql", "root:mysql1234@tcp(localhost:3306)/dashboard")
-	if err != nil {
-		log.Panic(err)
-	}
-	defer db.Close()
+	database.Getdb()
 
 	var dir string
 	flag.StringVar(&dir, "dir", "./ui/build", "server for UI")
 	flag.Parse()
 	r.PathPrefix("/home").Handler(http.StripPrefix("/home", http.FileServer(http.Dir(dir))))
 
-	r.HandleFunc("/signup", controller.HandleSignUp)
-	r.HandleFunc("/signin", controller.HandleSignIn)
+	r.HandleFunc("/signup", handlers.HandleSignUp)
+	r.HandleFunc("/signin", handlers.HandleSignIn)
 
 	log.Printf("Server Started")
-	http.ListenAndServe(":8000", r)
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
