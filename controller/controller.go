ef6f91b0c1c606268f53d5617129db9547daf361
AAYUSH MITTAL <aayushmittal1998@gmail.com>
2019-07-15 22:08:14 +0530

    Add files via upload

--- controller/controller.go
+++ controller/controller.go
@@ -15,32 +15,76 @@ var err error
 //HandleSignUp func
 func HandleSignUp(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
 	var user model.User
-	stmt, err := db.Prepare("INSERT INTO users(Username,FirstName,LastName,Password,Gender,Country,Age,Email) VALUES(?)")
-	if err != nil {
-		log.Fatal(err)
-	}
+	var query string
+	var res model.ResponseResult
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	err = json.Unmarshal(body, &user)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
-	_, err = stmt.Exec(user.Username, user.FirstName, user.LastName, user.Password, user.Gender, user.Country, user.Age, user.Email)
+	stmtOut, err := db.Prepare("SELECT username FROM users WHERE username  = ? ")
 	if err != nil {
-		log.Fatal(err)
+		panic(err.Error())
 	}
+	defer stmtOut.Close()
+	err = stmtOut.QueryRow(user.Username).Scan(&query)
+	if err != nil {
+		stmt, err := db.Prepare("INSERT INTO users(Username,FirstName,LastName,Password,Gender,Country,Age,Email) VALUES(?)")
+		if err != nil {
+			log.Panic(err)
+		}
+		defer stmt.Close()
 
-	log.Print("new user is created.")
+		_, err = stmt.Exec(user.Username, user.FirstName, user.LastName, user.Password, user.Gender, user.Country, user.Age, user.Email)
+		if err != nil {
+			log.Panic(err)
+		}
+		res.Result = "Registration Successful"
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+	res.Error = "Already Registered Username"
+	json.NewEncoder(w).Encode(res)
+
 }
 
 //HandleSignIn func
 func HandleSignIn(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var user model.User
+	var query model.User
+	var res model.ResponseResult
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	stmtOut, err := db.Prepare("SELECT username FROM users WHERE (username  = ? OR email = ?) AND password =?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmtOut.Close()
+	err = stmtOut.QueryRow(user.Email, user.Email, user.Password).Scan(&query)
+	if err != nil {
+		res.Error = "Invalid Email/Password"
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+	res.Result = "Login Successful"
+	json.NewEncoder(w).Encode(res)
 
 }
