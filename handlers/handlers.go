91acb0a1c6fe8f320509437240be3e34bd691fd9
AAYUSH MITTAL <aayushmittal1998@gmail.com>
2019-07-28 14:37:19 +0530

    initial_sign_in

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -33,3 +33,28 @@ func HandleSignUp(w http.ResponseWriter, r *http.Request) {
 
 	}
 }
+
+//HandleSignIn func
+func HandleSignIn(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var user model.UserProfile
+	var res string
+	var err error
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Print(err)
+	}
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		log.Print(err)
+	}
+
+	err = database.LoginUser(&user)
+	if err == nil {
+		res = "Log In Successful"
+		json.NewEncoder(w).Encode(res)
+	}
+}
