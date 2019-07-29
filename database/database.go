a7eb7188380c8df759956cfb715ac5652692367b
AAYUSH MITTAL <aayushmittal1998@gmail.com>
2019-07-29 21:26:37 +0530

    updated_token_generator

--- database/database.go
+++ database/database.go
@@ -4,6 +4,8 @@ import (
 	"dashboard/model"
 	"database/sql"
 	"log"
+	"math/rand"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -24,17 +26,18 @@ func CloseDb() {
 }
 
 //GetUserByUsername func to get user by username
-func GetUserByUsername(username string) (*model.UserProfile, error) {
+func GetUserByUsername(username string) (*model.UserProfile, int, error) {
 	var err error
 	var profile *model.UserProfile
+	var UID int
 	stmtOut, err := db.Prepare("SELECT * FROM user_profile WHERE username  = ? ")
 	if err != nil {
 		log.Print(err)
-		return nil, err
+		return nil, 0, err
 	}
 	defer stmtOut.Close()
-	err = stmtOut.QueryRow(username).Scan(&profile.Username, &profile.FirstName, &profile.LastName, &profile.Password, &profile.Gender, &profile.Country, &profile.Age, &profile.Email)
-	return profile, err
+	err = stmtOut.QueryRow(username).Scan(&UID, &profile.Username, &profile.FirstName, &profile.LastName, &profile.Password, &profile.Gender, &profile.Country, &profile.Age, &profile.Email)
+	return profile, UID, err
 }
 
 //EncryptPassword to secure passwords
@@ -53,15 +56,14 @@ func InsertUserProfile(user *model.UserProfile) error {
 	var profile *model.UserProfile
 	var hashedpassword string
 	var err error
-	profile, err = GetUserByUsername(user.Username)
+	profile, _, err = GetUserByUsername(user.Username)
 	if profile == nil && err != nil {
 		hashedpassword, err = EncryptPassword(user.Password)
-		if err == nil {
-			user.Password = hashedpassword
-		} else {
+		if err != nil {
 			log.Print(err)
 			return err
 		}
+		user.Password = hashedpassword
 		stmt, err := db.Prepare("INSERT INTO user_profile(Username,FirstName,LastName,Password,Gender,Country,Age,Email) VALUES(?,?,?,?,?,?,?,?)")
 		if err != nil {
 			log.Print(err)
@@ -75,48 +77,60 @@ func InsertUserProfile(user *model.UserProfile) error {
 			return err
 		}
 	}
-	return err
+	return nil
 }
 
-//GenerateToken func to add token in db
-func GenerateToken(username string) error {
+//InsertUserAuth to insert token in db.
+func InsertUserAuth(UID int, Token string) error {
 	var userAuth *model.UserAuth
-	var hashedtoken string
-	var err error
-	hashedtoken, err = EncryptPassword(username)
-	if err == nil {
-		userAuth.Token = hashedtoken
-	} else {
-		log.Print(err)
-		return err
-	}
-	userAuth.Username = username
-	stmt, err := db.Prepare("INSERT INTO user_auth(Username,Token) VALUES(?,?)")
+	userAuth.UID = UID
+	userAuth.Token = Token
+	userAuth.TimeGenerated = time.Now()
+	stmt, err := db.Prepare("INSERT INTO user_auth(UID,Token,TimeGenerated) VALUES(?,?,?)")
 	if err != nil {
 		log.Print(err)
 		return err
 	}
 	defer stmt.Close()
-	_, err = stmt.Exec(userAuth.Username, userAuth.Token)
+	_, err = stmt.Exec(userAuth.UID, userAuth.Token, userAuth.TimeGenerated)
 	if err != nil {
 		log.Print(err)
 		return err
 	}
 	return nil
+
+}
+
+//GenerateToken func to generate random token string
+func GenerateToken() string {
+	bytes := make([]byte, 25)
+	for i := 0; i < 25; i++ {
+		bytes[i] = byte(65 + rand.Intn(25)) //A=65 and Z = 65+25
+	}
+	return string(bytes)
 }
 
 //LoginUser func for sign_in
-func LoginUser(user *model.UserProfile) error {
+func LoginUser(user *model.UserProfile) (string, error) {
 	var profile *model.UserProfile
+	var token string
+	var UID int
 	var err error
-	profile, err = GetUserByUsername(user.Username)
-	if err == nil {
-		err = bcrypt.CompareHashAndPassword([]byte(profile.Password), []byte(user.Password))
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		err = GenerateToken(user.Username)
+	profile, UID, err = GetUserByUsername(user.Username)
+	if err != nil {
+		log.Print(err)
+		return "", err
 	}
-	return nil
+	err = bcrypt.CompareHashAndPassword([]byte(profile.Password), []byte(user.Password))
+	if err != nil {
+		log.Print(err)
+		return "", err
+	}
+	token = GenerateToken()
+	err = InsertUserAuth(UID, token)
+	if err != nil {
+		log.Print(err)
+		return "", err
+	}
+	return token, nil
 }
